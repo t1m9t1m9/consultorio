1e81fb2f167d1357a3bd1cc29556da139949a8f1
Marcelo Espinosa <t1m9@protonmail.com>
2021-05-12 20:45:05 -0500

    se agrego la clase CDbConnection para realizar las consultas y validar el login con la base de datos

--- protected/config/database.php
+++ protected/config/database.php
@@ -2,9 +2,9 @@
 
 // This is the database connection configuration.
 return array(
-	'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
+//	'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 	// uncomment the following lines to use a MySQL database
-
+    'class' => 'CDbConnection',
 	'connectionString' => 'mysql:host=localhost;dbname=consultorio',
 	'emulatePrepare' => true,
 	'username' => 'root',
