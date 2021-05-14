f7fcabbb57560820c3e36faded1552a0b5a3572a
Marcelo Espinosa <t1m9@protonmail.com>
2021-05-14 18:37:55 -0500

    Se realiza la configuracion necesaria para habilitar bootstrap

--- protected/config/main.php
+++ protected/config/main.php
@@ -1,5 +1,5 @@
 <?php
-
+Yii::setPathOfAlias('bootstrap', dirname(__FILE__).'/../extensions/bootstrap');
 // uncomment the following to define a path alias
 // Yii::setPathOfAlias('local','path/to/local-folder');
 
@@ -8,7 +8,7 @@
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
 	'name'=>'Consultorio Medico Gutierrez',
-
+    'theme'=>'bootstrap',
 	// preloading 'log' component
 	'preload'=>array('log'),
 
@@ -19,6 +19,11 @@ return array(
 	),
 
 	'modules'=>array(
+
+        'gii'=>array(
+            'generatorPaths'=>array(
+                'bootstrap.gii',
+            ),),
 		// uncomment the following to enable the Gii tool
 
 		'gii'=>array(
@@ -32,7 +37,9 @@ return array(
 
 	// application components
 	'components'=>array(
-
+            'bootstrap'=>array(
+            'class'=>'bootstrap.components.Bootstrap',
+        ),
 		'user'=>array(
 			// enable cookie-based authentication
 			'allowAutoLogin'=>true,
