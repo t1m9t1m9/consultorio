eea4669256c98e5dea9aea28c73f75f8780d8f58
Marce <t1m9@protonmail.com>
2022-02-11 21:25:53 -0500

    se arreglos el mostrar la cita agendada del usuario y la confirmacion

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -2,134 +2,124 @@
 
 class SiteController extends Controller
 {
-	/**
-	 * Declares class-based actions.
-	 */
-	public function actions()
-	{
-		return array(
-			// captcha action renders the CAPTCHA image displayed on the contact page
-			'captcha'=>array(
-				'class'=>'CCaptchaAction',
-				'backColor'=>0xFFFFFF,
-			),
-			// page action renders "static" pages stored under 'protected/views/site/pages'
-			// They can be accessed via: index.php?r=site/page&view=FileName
-			'page'=>array(
-				'class'=>'CViewAction',
-			),
-		);
-	}
+    /**
+     * Declares class-based actions.
+     */
+    public function actions()
+    {
+        return array(
+            // captcha action renders the CAPTCHA image displayed on the contact page
+            'captcha' => array(
+                'class' => 'CCaptchaAction',
+                'backColor' => 0xFFFFFF,
+            ),
+            // page action renders "static" pages stored under 'protected/views/site/pages'
+            // They can be accessed via: index.php?r=site/page&view=FileName
+            'page' => array(
+                'class' => 'CViewAction',
+            ),
+        );
+    }
 
-	/**
-	 * This is the default 'index' action that is invoked
-	 * when an action is not explicitly requested by users.
-	 */
-	public function actionIndex()
-	{
-		// renders the view file 'protected/views/site/index.php'
-		// using the default layout 'protected/views/layouts/main.php'
-		$this->render('index');
-	}
+    /**
+     * This is the default 'index' action that is invoked
+     * when an action is not explicitly requested by users.
+     */
+    public function actionIndex()
+    {
+        // renders the view file 'protected/views/site/index.php'
+        // using the default layout 'protected/views/layouts/main.php'
+        $this->render('index');
+    }
 
-	/**
-	 * This is the action to handle external exceptions.
-	 */
-	public function actionError()
-	{
-		if($error=Yii::app()->errorHandler->error)
-		{
-			if(Yii::app()->request->isAjaxRequest)
-				echo $error['message'];
-			else
-				$this->render('error', $error);
-		}
-	}
+    /**
+     * This is the action to handle external exceptions.
+     */
+    public function actionError()
+    {
+        if ($error = Yii::app()->errorHandler->error) {
+            if (Yii::app()->request->isAjaxRequest)
+                echo $error['message'];
+            else
+                $this->render('error', $error);
+        }
+    }
 
-	/**
-	 * Displays the contact page
-	 */
-	public function actionContact()
-	{
-		$model=new ContactForm;
-		if(isset($_POST['ContactForm']))
-		{
-			$model->attributes=$_POST['ContactForm'];
-			if($model->validate())
-			{
-				$name='=?UTF-8?B?'.base64_encode($model->name).'?=';
-				$subject='=?UTF-8?B?'.base64_encode($model->subject).'?=';
-				$headers="From: $name <{$model->email}>\r\n".
-					"Reply-To: {$model->email}\r\n".
-					"MIME-Version: 1.0\r\n".
-					"Content-Type: text/plain; charset=UTF-8";
+    /**
+     * Displays the contact page
+     */
+    public function actionContact()
+    {
+        $model = new ContactForm;
+        if (isset($_POST['ContactForm'])) {
+            $model->attributes = $_POST['ContactForm'];
+            if ($model->validate()) {
+                $name = '=?UTF-8?B?' . base64_encode($model->name) . '?=';
+                $subject = '=?UTF-8?B?' . base64_encode($model->subject) . '?=';
+                $headers = "From: $name <{$model->email}>\r\n" .
+                    "Reply-To: {$model->email}\r\n" .
+                    "MIME-Version: 1.0\r\n" .
+                    "Content-Type: text/plain; charset=UTF-8";
 
-				mail(Yii::app()->params['adminEmail'],$subject,$model->body,$headers);
-				Yii::app()->user->setFlash('contact','Thank you for contacting us. We will respond to you as soon as possible.');
-				$this->refresh();
-			}
-		}
-		$this->render('contact',array('model'=>$model));
-	}
+                mail(Yii::app()->params['adminEmail'], $subject, $model->body, $headers);
+                Yii::app()->user->setFlash('contact', 'Thank you for contacting us. We will respond to you as soon as possible.');
+                $this->refresh();
+            }
+        }
+        $this->render('contact', array('model' => $model));
+    }
 
-	/**
-	 * Displays the login page
-	 */
-	public function actionLogin()
-	{
-		$model=new LoginForm;
+    /**
+     * Displays the login page
+     */
+    public function actionLogin()
+    {
+        $model = new LoginForm;
 
-		// if it is ajax validation request
-		if(isset($_POST['ajax']) && $_POST['ajax']==='login-form')
-		{
-			echo CActiveForm::validate($model);
-			Yii::app()->end();
-		}
+        // if it is ajax validation request
+        if (isset($_POST['ajax']) && $_POST['ajax'] === 'login-form') {
+            echo CActiveForm::validate($model);
+            Yii::app()->end();
+        }
 
-		// collect user input data
-		if(isset($_POST['LoginForm']))
-		{
-			$model->attributes=$_POST['LoginForm'];
-			// validate user input and redirect to the previous page if valid
-			if($model->validate() && $model->login())
-				$this->redirect(Yii::app()->user->returnUrl);
-		}
-		// display the login form
-		$this->render('login',array('model'=>$model));
-	}
+        // collect user input data
+        if (isset($_POST['LoginForm'])) {
+            $model->attributes = $_POST['LoginForm'];
+            // validate user input and redirect to the previous page if valid
+            if ($model->validate() && $model->login())
+                $this->redirect(Yii::app()->user->returnUrl);
+        }
+        // display the login form
+        $this->render('login', array('model' => $model));
+    }
 
-	/**
-	 * Logs out the current user and redirect to homepage.
-	 */
-	public function actionLogout()
-	{
-		Yii::app()->user->logout();
-		$this->redirect(Yii::app()->homeUrl);
-	}
+    /**
+     * Logs out the current user and redirect to homepage.
+     */
+    public function actionLogout()
+    {
+        Yii::app()->user->logout();
+        $this->redirect(Yii::app()->homeUrl);
+    }
 
-	//REGISTRO DE USUARIO
-	public function actionregistro()
+    //REGISTRO DE USUARIO
+    public function actionregistro()
     {
         $model = new ValidarRegistro;
         $msg = '';
 //        $model->sexo = '1';
 //        $model->etnia = '1';
         // if it is ajax validation request
-        if(isset($_POST['ajax']) && $_POST['ajax']==='form')
-        {
+        if (isset($_POST['ajax']) && $_POST['ajax'] === 'form') {
             echo CActiveForm::validate($model);
             Yii::app()->end();
         }
 
-        if(isset($_POST['ValidarRegistro']))
-        {
-            $model->attributes=$_POST['ValidarRegistro'];
-            if(!$model->validate())
-            {
+        if (isset($_POST['ValidarRegistro'])) {
+            $model->attributes = $_POST['ValidarRegistro'];
+            if (!$model->validate()) {
                 $model->addError('repetir_password', 'Error al enviar el Formulario');
-            }
-            else
-            {
+            } else {
                 //Guardar el nuevo usuario en la base de datos
                 $guardar = new ConsultasBD;
                 $guardar->guardarUsuario(
@@ -150,29 +140,29 @@ class SiteController extends Controller
                     $model->password
                 );
 
-                    //Refrescar Pagina
-                    $model->ci = '';
-                    $model->nombre1 = '';
+                //Refrescar Pagina
+                $model->ci = '';
+                $model->nombre1 = '';
 //                    $model->nombre2 = '';
-                    $model->apellido1 = '';
+                $model->apellido1 = '';
 //                    $model->apellido2 = '';
 //                    $model->edad = '';
 //                    $model->sexo = '';
 //                    $model->etnia = '';
-                    $model->email = '';
+                $model->email = '';
 //                    $model->celular = '';
 //                    $model->direccion = '';
 //                    $model->pregunta1 = '';
 //                    $model->pregunta2 = '';
 //                    $model->pregunta3 = '';
-                    $model->password = '';
-                    $model->repetir_password = '';
+                $model->password = '';
+                $model->repetir_password = '';
 
-                    //Mensaje de confirmacion de registro
-                    $msg = 'Se registro exitosamente';
+                //Mensaje de confirmacion de registro
+                $msg = 'Se registro exitosamente';
             }
         }
-             $this->render('registro', array('model'=>$model, 'msg'=> $msg));
+        $this->render('registro', array('model' => $model, 'msg' => $msg));
     }
 
     //valida una pagina para que solo se tenga acceso si estas logeado en el sistema
@@ -195,43 +185,36 @@ class SiteController extends Controller
         $msg1 = '';
         $password = '';
 
-        if(isset($_POST["RecuperarPassword"]))
-        {
+        if (isset($_POST["RecuperarPassword"])) {
             $model->attributes = $_POST['RecuperarPassword'];
-            if(!$model->validate())
-            {
+            if (!$model->validate()) {
                 $msg = "<strong class='text-error'>Error al enviar el formulario</strong>";
-            }
-            else
-            {
+            } else {
                 $conexion = Yii::app()->db;
 
                 //Verificar si el usuario existe
                 $consulta = "SELECT ci, email FROM usuario WHERE ";
-                $consulta .= "ci='".$model->username."' AND email='$model->email'";
+                $consulta .= "ci='" . $model->username . "' AND email='$model->email'";
 
                 $resultado = $conexion->createCommand($consulta);
                 $filas = $resultado->query();
                 $existe = false;
 
-                foreach ($filas as $fila)
-                {
+                foreach ($filas as $fila) {
                     $existe = true;
                 }
 
                 //si el usuario existe
-                if($existe === true)
-                {
+                if ($existe === true) {
                     //buscar el password del usuario
                     $consulta = "SELECT password FROM usuario WHERE ";
-                    $consulta .= "ci='".$model->username."' AND email='".$model->email."'";
+                    $consulta .= "ci='" . $model->username . "' AND email='" . $model->email . "'";
 
                     $resultado = $conexion->createCommand($consulta)->query();
 
                     $resultado->bindColumn(1, $password);
 
-                    while($resultado->read()!==false)
-                    {
+                    while ($resultado->read() !== false) {
                         $actualizar1 = new ConsultasBD;
                         $actualizar1->actualizar_pass(
                             $model->username
@@ -243,21 +226,19 @@ class SiteController extends Controller
                         //refrescar vista
                         $model->username = '';
                         $model->email = '';
-                        $model->captcha ='';
+                        $model->captcha = '';
                     }
                 }
             }
         }
-        $this->render('recuperarpassword', array('model'=>$model, 'msg' => $msg, 'msg1' => $msg1));
+        $this->render('recuperarpassword', array('model' => $model, 'msg' => $msg, 'msg1' => $msg1));
     }
 
     public function actionAgendarCita()
     {
-        if(Yii::app()->user->isGuest)
-        {
+        if (Yii::app()->user->isGuest) {
             $this->redirect(Yii::app()->homeUrl);
-        }
-        else {
+        } else {
             $model = new AgendarCita;
             $msg = "";
             if (isset($_POST['ajax']) && $_POST['ajax'] === 'form') {
@@ -285,14 +266,14 @@ class SiteController extends Controller
             }
             $this->render('agendarcita', array('model' => $model, 'msg' => $msg));
         }
-        }
+    }
 
     public function actionCalendario()
     {
         if (Yii::app()->user->isGuest)
-        $this->redirect(Yii::app()->homeUrl);
+            $this->redirect(Yii::app()->homeUrl);
         else
-        $this->render('calendario');
+            $this->render('calendario');
     }
 
     public function actionCalendarioDoctora()
--- protected/views/site/calendario.php
+++ protected/views/site/calendario.php
@@ -1,41 +1,13 @@
 <?php
 $this->pageTitle=Yii::app()->name . ' - Calendario';
 $this->breadcrumbs=array('Calendario');
-//$cedu=Yii::app()->user->name;
-//$idusu = Usuario::model()->findAll();
-//$i1 = 1;
-//$posision = 0;
-//foreach($idusu as $idusuced)
-//{
-//    if($cedu == $ced[$i1++]="$idusuced->ci")
-//    {
-//        $posision=$i1;
-//    }
-//    $nom[$i1]="$idusuced->primerNombre";
-//    $ape[$i1]="$idusuced->primerApellido";
-//}?>
-<!--<br>-->
-<?php
-//$citas = Agenda::model()->findAll();
-//$i1 = 1;
-//$posision = 0;
-//foreach ($citas as $cita)
-//{
-//    if($cedu == $ced[$i1++]="$cita->ciUsuario")
-//    {
-//        $posision=$i1;
-//    }
-//    $fecha[$i1]="$cita->fecha";
-////    echo "T";
-//    $hora[$i1]="$cita->hora";
-//}
-//echo $fecha[$posision];
-//echo "T";
-//echo $hora[$posision];
 ?>
 
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/combine/npm/fullcalendar@5.7.0/main.min.css,npm/fullcalendar@5.7.0/main.min.css">
 <script src='https://cdn.jsdelivr.net/combine/npm/fullcalendar@5.7.0,npm/fullcalendar@5.7.0/locales-all.min.js,npm/fullcalendar@5.7.0/locales-all.min.js,npm/fullcalendar@5.7.0/main.min.js'></script>
+<style>
+    #agenda{max-width: 900px; margin: 10px auto;}
+</style>
 <script>
     document.addEventListener('DOMContentLoaded', function()
         {
@@ -45,28 +17,22 @@ $this->breadcrumbs=array('Calendario');
                     initialView: 'dayGridMonth',
                     locale: 'es',
                     headerToolbar: {left: '', center: 'title', right: ''},
-                    // headerToolbar: {
-                    //     left: '',
-                    //     // left: 'prev,next today',
-                    //     center: 'title',
-                    //     locale: 'es',
-                    //     right: 'dayGridMonth,timeGridWeek,timeGridDay'
-                    //     //right: ''
-                    // },
                 });
             calendar.addEvent({title: 'CITA MEDICA',
                     start: '<?php
                         $cedu = Yii::app()->user->name;
                         $citas = Agenda::model()->findAll();
                         $i1 = 1;
+                        $i2 = 1;
                         $posision = 0;
-
                         foreach ($citas as $cita)
                         {
-                            if($cedu == $ced[$i1++]="$cita->ciUsuario" && $cita->confirmacion == "SI")
+                            if($cedu == $ced[$i1++]="$cita->ciUsuario")
                             {
-                               $posision=$i1;
-
+                                if("SI" == $agenda[$i2++]="$cita->confirmacion")
+                                {
+                                    $posision=$i1;
+                                }
                             }
                             $fecha[$i1]="$cita->fecha";
                             $hora[$i1]="$cita->hora";
@@ -77,9 +43,7 @@ $this->breadcrumbs=array('Calendario');
                         }
                         else
                         {
-                            echo $fecha[$posision];
-                            echo "T";
-                            echo $hora[$posision];
+                            echo $fecha[$posision]."T".$hora[$posision];
                         }
                         ?>',
                     color: 'red'});
@@ -90,13 +54,3 @@ $this->breadcrumbs=array('Calendario');
 <div class="container">
     <div id="agenda"></div>
 </div>
-<!--<div class="container">-->
-<!--    <div class="row">-->
-<!--        <div class="col"></div>-->
-<!--        <div class="col-7"> <div id='agenda'></div></div>-->
-<!--        <div class="col"></div>-->
-<!--    </div>-->
-<!--</div>-->
-
-
-<br>
