e5944cfbadd8f487878d3d1bcf88cd58a408c8ec
Marce <t1m9@protonmail.com>
2022-02-14 19:53:09 -0500

    se corrigio el error de footer

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -170,7 +170,7 @@ foreach($idusu as $idusuced)
 <!--		Copyright &copy; --><?php //echo date('Y'); ?><!-- by Liz Jurado.<br/>-->
 <!--		All Rights Reserved.<br/>-->
 <!--		--><?php //echo Yii::powered(); ?>
-<!--	</div><!-- footer -->-->
+<!--	</div><!-- footer -->
 
 </div><!-- page -->
 
